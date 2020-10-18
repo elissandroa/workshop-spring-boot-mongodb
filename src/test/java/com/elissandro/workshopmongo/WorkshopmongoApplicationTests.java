a81c6624e573da240a668005e74dd6b93df96aef
Elissandro <elissandroa@hotmail.com>
2020-10-17 22:36:05 -0300

    Projeto Criado

---
+++ src/test/java/com/elissandro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elissandro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
